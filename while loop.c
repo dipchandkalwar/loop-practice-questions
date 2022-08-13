05b2db208b12f5e5ce50a26f0147d669f0939e12
Dipchand kalwar <111203713+shinelikeasun@users.noreply.github.com>
2022-08-14 03:46:27 +0530

    Add files via upload

---
+++ while loop.c
@@ -0,0 +1,24 @@
+/*#include<stdio.h>
+int main(){
+int n;
+printf("enter the value of n:");
+scanf("%d",&n);
+int i=0;
+while(i<=n){
+	printf("%d\n",i);
+	i++;
+}
+return 0;
+}*/
+#include<stdio.h>
+int main(){
+    int i;
+int n;
+printf("enter the value of n:");
+scanf("%d",&n);
+for (  i=0;i<=n;i++){
+    printf("%d",i);
+}
+return 0;
+
+}	
